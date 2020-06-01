8a2ca33738a3d3bbeec591af42bb8d4b3dce7ff5
shwetatalreja <shwetatalreja96@gmail.com>
2020-06-01 21:31:25 +0530

    check

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Frontend
+          Learn Fron
           
         </a>
       </header>
